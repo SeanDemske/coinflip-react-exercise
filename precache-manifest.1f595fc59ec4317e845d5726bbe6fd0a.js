706b31ba133a064fcc816b5a673af09f0be3bb67
Sean Demske <seandemske@gmail.com>
2020-08-06 15:01:02 -0400

    Updates

---
+++ precache-manifest.1f595fc59ec4317e845d5726bbe6fd0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5da1bd4558e0b83340d8650099521ec",
+    "url": "/coinflip-react-exercise/index.html"
+  },
+  {
+    "revision": "3edcbd9ddd781e942c05",
+    "url": "/coinflip-react-exercise/static/css/main.d7f5076c.chunk.css"
+  },
+  {
+    "revision": "507e9fe8803ecf19bea8",
+    "url": "/coinflip-react-exercise/static/js/2.db11a6a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/coinflip-react-exercise/static/js/2.db11a6a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3edcbd9ddd781e942c05",
+    "url": "/coinflip-react-exercise/static/js/main.e9b045c4.chunk.js"
+  },
+  {
+    "revision": "85f23c3343b3bc5ecb7a",
+    "url": "/coinflip-react-exercise/static/js/runtime-main.037a6bfb.js"
+  }
+]);
